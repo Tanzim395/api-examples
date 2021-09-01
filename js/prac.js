d3e49909dffdc94f6dad0f31e12d9d5c7cd4ffb9
Tanzim395 <junaidulkader@gmail.com>
2021-09-01 20:01:38 +0600

    first commit

---
+++ js/prac.js
@@ -0,0 +1,7 @@
+// const data = { result: [{ userName: { title: 'Mr.', name: "sakib khan" } }] }
+// const res = data.result[0].userName.name;
+// console.log(res);
+
+// const p = [1, 2, 3];
+// const q = p.map(n => Math.pow(n, 3));
+// console.log(q);
